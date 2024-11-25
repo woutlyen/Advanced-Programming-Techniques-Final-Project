74ab2e82f83a9a7c03d4792b058a96ad99f9b09a
Wout Lyen <woutlyen@gmail.com>
2024-11-25 09:47:55 +0100

    Written code for the ProtagtonistView2D

--- FinalProject/View/healthpackview2d.cpp
+++ FinalProject/View/healthpackview2d.cpp
@@ -1,3 +1,8 @@
 #include "healthpackview2d.h"
 
-HealthPackView2D::HealthPackView2D() {}
+HealthPackView2D::HealthPackView2D(const std::unique_ptr<Tile>& healthPack, std::size_t gridSize, QGraphicsItem* parent)
+    : QGraphicsPixmapItem(parent) {
+
+    setPixmap(QPixmap(":/images/heart.png").scaled(gridSize,gridSize));
+    setPos(gridSize*healthPack->getXPos(), gridSize*healthPack->getYPos());
+}
--- FinalProject/View/healthpackview2d.h
+++ FinalProject/View/healthpackview2d.h
@@ -1,12 +1,14 @@
 #ifndef HEALTHPACKVIEW2D_H
 #define HEALTHPACKVIEW2D_H
 
-#include "healthpackview.h"
+#include <QGraphicsPixmapItem>
 
-class HealthPackView2D : public HealthPackView
+#include "world.h"
+
+class HealthPackView2D : public QGraphicsPixmapItem
 {
 public:
-    HealthPackView2D();
+    HealthPackView2D(const std::unique_ptr<Tile>& healthPack, std::size_t gridSize, QGraphicsItem* parent = nullptr);
 };
 
 #endif // HEALTHPACKVIEW2D_H
