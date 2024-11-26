80a10e8ee946293236c869a0c09566615b3caa7c
Wout Lyen <woutlyen@gmail.com>
2024-11-26 21:01:18 +0100

    Updated Energy calculation

--- FinalProject/Controller/playercontroller.cpp
+++ FinalProject/Controller/playercontroller.cpp
@@ -3,7 +3,7 @@
 PlayerController::PlayerController() {}
 
 void PlayerController::moveUp(std::unique_ptr<Protagonist>& protagonist, std::vector<std::unique_ptr<Tile>>& tiles, int width){
-    if (protagonist->getYPos() > 0 && protagonist->getEnergy() > 0){
+    if (protagonist->getYPos() > 0 && protagonist->getEnergy() >= 1){
         protagonist->setEnergy(protagonist->getEnergy()
                                - (tiles.at((protagonist->getYPos()-1)*width+protagonist->getXPos())->getValue()));
         protagonist->setYPos(protagonist->getYPos()-1);
@@ -11,7 +11,7 @@ void PlayerController::moveUp(std::unique_ptr<Protagonist>& protagonist, std::ve
 }
 
 void PlayerController::moveDown(std::unique_ptr<Protagonist>& protagonist, std::vector<std::unique_ptr<Tile>>& tiles, int width, int height){
-    if (protagonist->getYPos() < height-1 && protagonist->getEnergy() > 0){
+    if (protagonist->getYPos() < height-1 && protagonist->getEnergy() >= 1){
         protagonist->setEnergy(protagonist->getEnergy()
                                - (tiles.at((protagonist->getYPos()+1)*width+protagonist->getXPos())->getValue()));
         protagonist->setYPos(protagonist->getYPos()+1);
@@ -19,7 +19,7 @@ void PlayerController::moveDown(std::unique_ptr<Protagonist>& protagonist, std::
 }
 
 void PlayerController::moveLeft(std::unique_ptr<Protagonist>& protagonist, std::vector<std::unique_ptr<Tile>>& tiles, int width){
-    if (protagonist->getXPos() > 0 && protagonist->getEnergy() > 0){
+    if (protagonist->getXPos() > 0 && protagonist->getEnergy() >= 1){
         protagonist->setEnergy(protagonist->getEnergy()
                                - (tiles.at(protagonist->getYPos()*width+protagonist->getXPos()-1)->getValue()));
         protagonist->setXPos(protagonist->getXPos()-1);
@@ -27,7 +27,7 @@ void PlayerController::moveLeft(std::unique_ptr<Protagonist>& protagonist, std::
 }
 
 void PlayerController::moveRight(std::unique_ptr<Protagonist>& protagonist, std::vector<std::unique_ptr<Tile>>& tiles, int width){
-    if (protagonist->getXPos() < width-1 && protagonist->getEnergy() > 0){
+    if (protagonist->getXPos() < width-1 && protagonist->getEnergy() >= 1){
         protagonist->setEnergy(protagonist->getEnergy()
                                - (tiles.at(protagonist->getYPos()*width+protagonist->getXPos()+1)->getValue()));
         protagonist->setXPos(protagonist->getXPos()+1);
