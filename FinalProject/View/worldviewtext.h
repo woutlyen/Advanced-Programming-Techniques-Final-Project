01eb258055c8ca9606ed90ac38272cd4e8aad55f
Wout Maes <wout.maes2@student.kuleuven.be>
2024-12-10 09:47:38 +0100

    Changed protagonist to player

--- FinalProject/View/worldviewtext.h
+++ FinalProject/View/worldviewtext.h
@@ -1,6 +1,7 @@
 #ifndef WORLDVIEWTEXT_H
 #define WORLDVIEWTEXT_H
 
+#include "Model/player.h"
 #include "View/statusbar2d.h"
 #include "View/protagonistviewtext.h"
 #include "world.h"
@@ -12,7 +13,7 @@ class WorldViewText {
     WorldViewText();
     QGraphicsScene *makeScene(std::vector<std::unique_ptr<Enemy>> &enemies,
                               std::vector<std::unique_ptr<Tile>> &healthPacks,
-                              std::unique_ptr<Protagonist> &protagonist,
+                              std::unique_ptr<Player> &protagonist,
                               int rows,
                               int columns,
                               QString filename = ":/world_images/worldmap.png",
