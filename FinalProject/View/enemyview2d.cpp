d6df5f6f40fa15d82d8836567cac4c3e3002c3c6
Vivian <viviantsang1310@gmail.com>
2024-12-21 17:27:47 +0100

    added initialization method

--- FinalProject/View/enemyview2d.cpp
+++ FinalProject/View/enemyview2d.cpp
@@ -1,29 +1,11 @@
 #include "enemyview2d.h"
 
 EnemyView2D::EnemyView2D(const std::unique_ptr<Enemy> &enemy, std::size_t gridSize)
-    : GameObject2DView(gridSize) {
+    : GameObject2DView(gridSize), enemy(enemy) {
 
-    // Initialize all Pixmaps
-    idlePixmaps = extractFrames(":/images/enemy_sprites/enemy_idle");
-    fightingPixmaps = extractFrames(":/images/enemy_sprites/enemy_attack"); //Unused for now
-    dyingPixmaps = extractFrames(":/images/enemy_sprites/enemy_die");
-    // Initialize Pixmap Animation Sizes
-    nrOfFramesIdle = idlePixmaps.size();
-    nrOfFramesWalking = 0;
-    nrOfFramesFighting = fightingPixmaps.size();
-    nrOfFramesDying = dyingPixmaps.size();
-
-    // Set the initial pixmap
-    setPixmap(idlePixmaps[0]);
-    setPos(gridSize*enemy->getXPos(), gridSize*enemy->getYPos());
-
+    initializeEnemy2DView();
     // Connect signals & slots
     connect(enemy.get(), &Enemy::dead, this, &EnemyView2D::onDefeated);
-
-    // Configure the animation timer
-    animationTimer->setInterval(1000); // Frame switch interval (in milliseconds)
-    animationTimer->start();
-    GameObject2DView::connectAnimationTimer();
 }
 
 void EnemyView2D::setAnimation()
@@ -44,6 +26,29 @@ void EnemyView2D::setAnimation()
     case Walking:
         break;
     }
+}
+
+void EnemyView2D::initializeEnemy2DView()
+{
+    // Initialize all Pixmaps
+    idlePixmaps = extractFrames(getIdlePixmapsPath());
+    fightingPixmaps = extractFrames(getFightingPixmapsPath()); //Unused for now
+    dyingPixmaps = extractFrames(getDyingPixmapsPath());
+
+    // Initialize Pixmap Animation Sizes
+    nrOfFramesIdle = idlePixmaps.size();
+    nrOfFramesWalking = 0;
+    nrOfFramesFighting = fightingPixmaps.size();
+    nrOfFramesDying = dyingPixmaps.size();
+
+    // Set the initial pixmap
+    setPixmap(idlePixmaps[0]);
+    setPos(gridSize*enemy->getXPos(), gridSize*enemy->getYPos());
+
+    // Configure the animation timer
+    animationTimer->setInterval(animationTimerInterval); // Frame switch interval (in milliseconds)
+    animationTimer->start();
+    GameObject2DView::connectAnimationTimer();
 }
 
 void EnemyView2D::onDefeated()
--- FinalProject/View/enemyview2d.h
+++ FinalProject/View/enemyview2d.h
@@ -2,7 +2,6 @@
 #define ENEMYVIEW2D_H
 
 #include "View/gameobject2dview.h"
-#include <QGraphicsPixmapItem>
 #include <world.h>
 
 class EnemyView2D : public GameObject2DView
@@ -10,13 +9,19 @@ class EnemyView2D : public GameObject2DView
 public:
     EnemyView2D(const std::unique_ptr<Enemy>& enemy, std::size_t gridSize);
 
-private:
+protected:
+    const std::unique_ptr<Enemy> &enemy;
     std::vector<QPixmap> idlePixmaps;
     std::vector<QPixmap> fightingPixmaps;
     std::vector<QPixmap> dyingPixmaps;
+    virtual QString getIdlePixmapsPath() const { return ":/images/enemy_sprites/enemy_idle"; }
+    virtual QString getFightingPixmapsPath() const { return ":/images/enemy_sprites/enemy_attack"; }
+    virtual QString getDyingPixmapsPath() const { return ":/images/enemy_sprites/enemy_die"; }
+    int animationTimerInterval = 500;
     void setAnimation();
+    void initializeEnemy2DView();
 private slots:
-    void onDefeated();
+    virtual void onDefeated();
 };
 
 #endif // ENEMYVIEW2D_H
